a1d427099bd2c98ff6a248caa320009043e85738
Fares Manai <fares.manaisademx@gmail.com>
2023-02-16 12:50:16 +0000

    QueryCacheService ignore transaction & count

--- ts/services/queryCache.ts
+++ ts/services/queryCache.ts
@@ -1,7 +1,7 @@
 import { config } from "../config";
 
 import { Router } from "express";
-import { FindOptions } from "sequelize";
+import { CountOptions, FindOptions, GroupedCountResultItem } from "sequelize";
 import { cloneDeep } from "lodash";
 
 import { KishiModel } from "../sequelize";
@@ -13,12 +13,11 @@ const logger = new FileLogger("queryCache")
 
 export class QueryCacheService {
 	static Init(models: { [name: string]: typeof KishiModel }, router: Router) {
-		async function _findAll(this: typeof KishiModel, options?: FindOptions | undefined): Promise<KishiModel[] | KishiModel | null> {
+		async function findAll(this: typeof KishiModel, options?: FindOptions | undefined): Promise<KishiModel[] | KishiModel | null> {
 			options = options || {}
 			let cacheObject: any = cloneDeep(options)
 			if (options.transaction) {
-				cacheObject.transactionId = (options.transaction as any).id
-				delete cacheObject.transaction
+				return await (this as any).cache_findAll(options) as KishiModel[] | KishiModel | null
 			}
 			const cacheKey = this.name + ":" + JSON.stringify(cacheObject)
 			const cache = await CacheLib.GetOrPromise(cacheKey, { timeout: 60 })
@@ -29,7 +28,7 @@ export class QueryCacheService {
 			const sub = cache as PromiseSub<[KishiModel[] | KishiModel | null, string[]]>
 			try {
 				const dependencies = this.FindOptionsToDependencies(options)
-				const result = await (this as any).__findAll(options) as KishiModel[] | KishiModel | null
+				const result = await (this as any).cache_findAll(options) as KishiModel[] | KishiModel | null
 				sub.resolve([result, dependencies])
 				return result
 			} catch (error) {
@@ -38,40 +37,64 @@ export class QueryCacheService {
 				throw error
 			}
 		}
+		async function count(this: typeof KishiModel, options?: CountOptions | undefined): Promise<number | GroupedCountResultItem[]> {
+			options = options || {}
+			let cacheObject: any = cloneDeep(options)
+			if (options.transaction) {
+				return await (this as any).cache_count(options) as number | GroupedCountResultItem[]
+			}
+			const cacheKey = this.name + ":" + JSON.stringify(cacheObject)
+			const cache = await CacheLib.GetOrPromise(cacheKey, { timeout: 60 })
+			if ("data" in (cache as CachePayLoad)) {
+				return cloneDeep((cache as CachePayLoad).data) as number | GroupedCountResultItem[]
+			}
+			const sub = cache as PromiseSub<[number | GroupedCountResultItem[], string[]]>
+			try {
+				const dependencies = this.FindOptionsToDependencies(options)
+				const result = await (this as any).cache_count(options) as number | GroupedCountResultItem[]
+				sub.resolve([result, dependencies])
+				return result
+			} catch (error) {
+				sub.reject(error)
+				throw error
+			}
+		}
 		for (const modelName in models) {
 			const model = (models[modelName]);
 			//Implement Cache
-			(model as any).__findAll = (model as any)._findAll.bind(model);
-			(model as any)._findAll = _findAll.bind(model);
+			(model as any).cache_findAll = (model as any).findAll.bind(model);
+			(model as any).findAll = findAll.bind(model);
+			(model as any).cache_count = (model as any).count.bind(model);
+			(model as any).count = count.bind(model);
 			model.afterCreate((row, options) => {
 				logger.warn(model.name, "afterCreate");
-				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name))
-				CacheLib.ClearCacheByTag(model.name)
+				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name)) ||
+					CacheLib.ClearCacheByTag(model.name)
 			})
 			model.afterUpdate((row, options) => {
 				logger.warn(model.name, "afterUpdate");
-				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name))
-				CacheLib.ClearCacheByTag(model.name)
+				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name)) ||
+					CacheLib.ClearCacheByTag(model.name)
 			})
 			model.afterDestroy((row, options) => {
 				logger.warn(model.name, "afterDestroy");
-				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name))
-				CacheLib.ClearCacheByTag(model.name)
+				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name)) ||
+					CacheLib.ClearCacheByTag(model.name)
 			})
 			model.afterBulkCreate((rows, options) => {
 				logger.warn(model.name, "afterBulkCreate");
-				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name))
-				CacheLib.ClearCacheByTag(model.name)
+				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name)) ||
+					CacheLib.ClearCacheByTag(model.name)
 			})
 			model.afterBulkUpdate((options) => {
 				logger.warn(model.name, "afterBulkUpdate");
-				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name))
-				CacheLib.ClearCacheByTag(model.name)
+				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name)) ||
+					CacheLib.ClearCacheByTag(model.name)
 			})
 			model.afterBulkDestroy((options) => {
 				logger.warn(model.name, "afterBulkDestroy");
-				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name))
-				CacheLib.ClearCacheByTag(model.name)
+				options.transaction?.afterCommit(() => CacheLib.ClearCacheByTag(model.name)) ||
+					CacheLib.ClearCacheByTag(model.name)
 			})
 		}
 	}
