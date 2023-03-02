1a2ed9c6f670d6d84ed9bc22d3e51b59dc9c3bac
Fares Manai <fares.manaisademx@gmail.com>
2023-03-02 13:07:44 +0000

    FIX index name too long

--- ts/models/ContractTemplate.ts
+++ ts/models/ContractTemplate.ts
@@ -77,6 +77,6 @@ export class ContractTemplate_Clause extends KishiModel {
     }
   }
   static initialOptions: KishiModelOptions = {
-    indexes: [{ fields: ["ContractTemplateId", "ClauseId", "index"], unique: true }]
+    indexes: [{ fields: ["ContractTemplateId", "ClauseId", "index"], unique: true,name:"ContractTemplate_Clause_index" }]
   }
 }
