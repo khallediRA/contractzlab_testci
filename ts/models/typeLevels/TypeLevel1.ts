c81953b1518c14b7a8764653dcd0ac61eee06673
Fares Manai <fares.manaisademx@gmail.com>
2023-04-26 09:57:46 +0100

    TypeLevels

--- ts/models/typeLevels/TypeLevel1.ts
+++ ts/models/typeLevels/TypeLevel1.ts
@@ -2,6 +2,53 @@ import { ModelHooks } from "sequelize/types/hooks";
 import { KishiModel, KishiModelAttributes, KishiDataTypes, KOp, typesOfKishiAssociationOptions, CrudOptions } from "../../sequelize";
 import { isOfType } from "../../utils/user";
 
+const initData = [
+  {
+    name: "Contrat",
+    levels2: [
+      {
+        name: "Achat",
+        levels3: [
+          { name: "Logiciel Saas" },
+          { name: "Service" },
+          { name: "Prestation scientifique" },
+          { name: "Immobilier" },
+        ]
+      },
+      {
+        name: "Vente",
+        levels3: []
+      },
+      {
+        name: "Travail",
+        levels3: []
+      },
+      {
+        name: "Licence",
+        levels3: []
+      },
+      {
+        name: "Maintenance",
+        levels3: []
+      },
+      {
+        name: "Partenariat",
+        levels3: []
+      },
+      {
+        name: "Protection des données personelles",
+        levels3: []
+      },
+    ]
+  },
+  { name: "SLA" },
+  { name: "Bon de commande" },
+  { name: "NDA" },
+  { name: "Conditions générales d'utilisation" },
+  { name: "Devis" },
+  { name: "Facture" },
+]
+
 export class TypeLevel1 extends KishiModel {
   static crudOptions: CrudOptions = {
     "create": (user) => (isOfType(user, "Admin", "Moderator")),
@@ -35,12 +82,23 @@ export class TypeLevel1 extends KishiModel {
       target: "TypeLevel2",
       foreignKey: "level1Id",
       schemaMap: {
+        "full": "nested",
+        "nested": "nested",
       },
+      actionMap: {
+        "Create": "Upsert",
+        "Update": "Upsert",
+      }
     },
   };
   static initialHooks: Partial<ModelHooks<KishiModel, any>> = {
+    async afterSync(options) {
+      console.log("TypeLevel1.afterBulkSync");
+      for (const row of initData) {
+        await TypeLevel1.Upsert(row)
+      }
+    },
     async beforeQuery(options, query) {
-      console.log(query);
     }
   }
 }
